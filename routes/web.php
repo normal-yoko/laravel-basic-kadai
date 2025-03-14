2f11a47e8a737971ecf571681bb9c1555f3087e5
normal-yoko <gijigiji399@gmail.com>
2025-03-14 23:25:22 +0900

    laravel-basic-kadaiをアップロード

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 //ルーティングを設定するコントローラを宣言する
-use App\Http\Controllers\HelloController
+use App\Http\Controllers\HelloController;
 use App\Http\Controllers\PostController;
 
 /*
